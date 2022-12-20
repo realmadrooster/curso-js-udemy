4b13570ed3309ef8d8d8a7f54016e4eb0f137472
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2022-12-20 19:20:28 -0300

    20-12-22

---
+++ aula34/index.js
@@ -0,0 +1,21 @@
+//console.log('linha 0');
+//console.log('linha 1');
+//console.log('linha 2');
+//console.log('linha 3');
+//console.log('linha 4');
+//console.log('linha 5');
+
+// i - index
+// for(let i = 0; i <= 5; i++){
+//     console.log(`linha ${i}`);
+// }
+// for(let i = 0; i <= 10; i++){
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i,par);
+// }
+
+const frutas = ['maçã','pera','uva'];
+
+for  (let i = 0; i < frutas.length; i++){
+    console.log(`indice ${i}`, frutas[i]);
+}
