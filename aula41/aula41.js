a8af6b4147465a9f58f4aaf25e882c0111b7a101
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2022-12-28 21:26:33 -0300

    28-12-22

---
+++ aula41/aula41.js
@@ -0,0 +1,17 @@
+//escreva uma funçao que recebe 2 numeros e retorne o maior deles
+
+
+// function max( x, y){
+//     if (x > y){
+//         return x;
+//     }
+//         return y;
+  
+
+
+// function max( x, y){
+//     return x > y ? x : y;
+// }
+
+const max2 = (x,y) => x > y ? x : y;
+console.log(`o maior é : ${max2(74, 35)}`);
