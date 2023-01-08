7375c26c2feb982584a475a09b0f2df3f828a298
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-01-08 16:21:40 -0300

    08-01-23

---
+++ aula52/aula52.js
@@ -0,0 +1,27 @@
+// const nome = 'luiz';
+
+// function falaNome() {
+//     console.log(nome);
+// }
+// falaNome();//imprime luiz
+
+// const nome = 'luiz';
+
+// function falaNome() {
+//     const nome = 'Carlos'
+//     console.log(nome);
+// }
+// falaNome();//imprime Carlos
+
+const nome = 'luiz';
+
+function falaNome() {
+   
+    console.log(nome);
+}
+
+function usaFalaNome() {
+    const nome = 'carlos';
+    falaNome();
+}
+usaFalaNome();//imprime luiz
