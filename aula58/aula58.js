4c900bed7266a3b47a59ab25aa942c8520b1a53b
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-01-13 20:26:17 -0300

    13-01-23

---
+++ aula58/aula58.js
@@ -0,0 +1,25 @@
+//funçao construtora -> objetos
+//funçao fabrica -> objetos
+//construtora -> new Pessoa
+
+function Pessoa(nome, sobrenome) {
+    //atributos ou metodos privados
+    const ID = 122345;//declarado com let ou const o atributo privado interno que so é visivel dentro do objeto nao pode ser acessado de fora
+    const metodoInterno = function() {
+
+    };//metodo interno que segue o mesmo padrao do atributo privado interno
+
+    //atributos ou metodo publicos
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodo = function() {
+        console.log(this.nome + ': Sou um metodo');
+    }
+}
+
+const p1 = new Pessoa('Luiz', 'Carlos');
+const p2 = new Pessoa('Maria', 'Clara');
+
+console.log(p1.nome);
+p1.metodo();
