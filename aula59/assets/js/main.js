bd0e8ac16f5d1e1bc33b80934a70acfbbec7d29d
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-01-14 17:44:58 -0300

    14-01-23 calc

---
+++ aula59/assets/js/main.js
@@ -0,0 +1,53 @@
+function Calculadora() {
+  this.display = document.querySelector('.display');
+
+  this.inicia = () => {
+    this.capturaCliques();
+    this.capturaEnter();
+  };
+  this.capturaEnter = () => {
+      document.addEventListener('keyup', e => {
+        if(e.keycode === 13) return;
+        this.realizaConta();
+      });
+    };
+
+  this.capturaCliques = () => {
+    document.addEventListener('click', event => {
+      const el = event.target;
+      if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+      if (el.classList.contains('btn-clear')) this.clear();
+      if (el.classList.contains('btn-del')) this.del(el);
+      if (el.classList.contains('btn-eq')) this.realizaConta();
+    });
+  };
+
+  this.realizaConta = () => {
+    try {
+      const conta = eval(this.display.value);
+
+      if(!conta) {
+        alert('conta invalida');
+        return;
+      }
+      this.display.value = conta;
+    } catch(e) {
+      alert('conta invalida');
+      return;
+    }
+  };
+
+  this.addNumDisplay = el => {
+    this.display.value += el.innerText;
+    this.display.focus();//tira o foco do botal que foi clicado pela ultima vez repassando o foco para o display
+  };
+
+  this.clear = () => this.display.value = '';
+
+
+  
+  this.del = () => this.display.value = this.display.value.slice(0, -1);
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
