908ad589c41533598b1571f10d7b72defd2a268d
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-02-01 20:36:49 -0300

    valida cpf

--- aula77/aula77.js
+++ aula77/aula77.js
@@ -19,7 +19,59 @@
 705.484.450-52 === 705.484.450-52 = true cpf valido
 */
 
-let cpf = '705.484.450-52';//tem que ser uma string senao podemos ter problemas com cpfs que começam com 0
-let cpfLimpo = cpf.replace(/\D+/g,'');//expressao regular que remove tudo que nao for digito 
-let cpfArray = Array.from(cpfLimpo);//converte a string em um array
-console.log(cpfArray.reduce((ac,val) => ac + Number(val), 0));
+// let cpf = '705.484.450-52';//tem que ser uma string senao podemos ter problemas com cpfs que começam com 0
+// let cpfLimpo = cpf.replace(/\D+/g,'');//expressao regular que remove tudo que nao for digito 
+// let cpfArray = Array.from(cpfLimpo);//converte a string em um array
+// console.log(cpfArray.reduce((ac,val) => ac + Number(val), 0));
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function() {
+            return cpfEnviado.replace(/\D+/g,'');
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2);//variavel com os 9 primeiros digitos do cpf
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+    
+    const novoCpf = cpfParcial + digito1 + digito2;
+
+    return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);//cria um array da string cpfParcial
+    
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+    
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);//cria uma sequencia que tem como base o primeiro caractere do atributo cpfLimpo
+    return sequencia === this.cpfLimpo;
+}
+
+const cpf = new ValidaCPF('070.987.720-03');
+//console.log(cpf.cpfLimpo);
+//cpf 111.111.111-11 é avaliado como true portanto deve ser verificado
+if(cpf.valida()) {
+    console.log('Cpf válido');
+}else {
+    console.log('Cpf inválido');
+}
+
