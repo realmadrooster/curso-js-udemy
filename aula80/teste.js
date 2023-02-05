a042e8b2ba61d71fc6633d195a9b72f6ba5eef1b
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-02-05 18:11:15 -0300

    Update teste.js

--- aula80/teste.js
+++ aula80/teste.js
@@ -1,3 +1,9 @@
 const teste = [1,2,3,4,5];
 const [a,b,...c] = teste;
 console.log(a,b,c);
+
+const teste2 = {galo:'ganhô',cruzeiro:'perdeu' };
+
+const {galo,cruzeiro} = teste2;
+console.log(galo);
+console.log(cruzeiro);
