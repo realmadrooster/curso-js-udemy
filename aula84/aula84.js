47ea733bb1095bde8c374720826630041f70fc18
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-02-08 22:15:57 -0300

    metodos estaticos

---
+++ aula84/aula84.js
@@ -0,0 +1,25 @@
+class ControleRemoto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+    }
+    aumentarVolume() {
+        this.volume += 2;
+    }
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+    //metodo estatico
+    static trocaPilha() {
+        console.log('OK, vou trocar');
+    }
+}
+
+const controle1 = new ControleRemoto('LG');
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
+
+//acessando metodo estatico
+ControleRemoto.trocaPilha();
