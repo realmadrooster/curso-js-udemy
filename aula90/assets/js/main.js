1c58d14f13b85207449e663af749f82b982a6fdc
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-02-14 21:33:14 -0300

    requisiçao ajax xmlhttprequest com promises

---
+++ aula90/assets/js/main.js
@@ -0,0 +1,91 @@
+//metodo antigo
+// const request = obj => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(obj.method, obj.url, true);
+//     xhr.send();
+
+//     xhr.addEventListener('load', () => {
+//         if(xhr.status >= 200 && xhr.status < 300) {
+//             obj.sucess(xhr.responseText);
+//         } else {
+//             obj.error(xhr.statusText);
+//         }
+//     });
+// };
+
+//metodo com promises
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
+
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status < 300) {
+            resolve(xhr.responseText);
+            } else {
+            reject(xhr.statusText);
+            }
+        });
+    })
+    
+};
+
+document.addEventListener('click', e => {
+    const el = e.target;//pega o elemento clicado
+    const tag = el.tagName.toLowerCase();//pega a tag do elemento clicado
+
+    if(tag === 'a') {
+        e.preventDefault();//quando clicar no link ele nao vai pro link
+        carregaPagina(el);
+    }
+});
+
+//metodo antigo com  callbacks
+
+// function carregaPagina(el) {
+//     const href = el.getAttribute('href');//pega o atributo href do link clicado
+//     request({
+//         method: 'GET',
+//         url: href,
+//         sucess(response) {
+//             carregaResultado(response);
+//         },
+//         error(errorText) {
+//             console.log(errorText);
+//         }
+//     });
+// }
+
+// //metodo novo com promises
+// function carregaPagina(el) {
+//     const href = el.getAttribute('href');//pega o atributo href do link clicado
+//     const objConfig = {
+//         method: 'GET',
+//         url: href,
+//         };
+//         request(objConfig).then(response => {
+//             carregaResultado(response);
+//         }).catch(error => console.log(error));
+    
+// }
+//metodo com promises e async
+async function carregaPagina(el) {
+    const href = el.getAttribute('href');//pega o atributo href do link clicado
+    const objConfig = {
+        method: 'GET',
+        url: href,
+        };
+        try {
+            const response = await request(objConfig);
+            carregaResultado(response); 
+        } catch(e) {
+            console.log(e);
+        }
+           
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
