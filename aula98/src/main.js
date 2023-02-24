88aa1a016495203e28cd428fb23f5a8702a7776e
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-02-23 21:24:26 -0300

    core-js and generator runtime para transpilar js em navegadores antigos

---
+++ aula98/src/main.js
@@ -0,0 +1,9 @@
+import 'core-js/stable';//juntamente com regenerator permite ao codigo js ser transpilado para navegadores antigos
+
+import 'regenerator-runtime/runtime';//faz com que o codigo js antigo seja executado
+
+import executa from './modules/promises';
+
+import './assets/css/style.css';
+
+executa();
---
+++ aula98/src/modules/promises.js
@@ -0,0 +1,15 @@
+import { async } from "regenerator-runtime";
+
+function promise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Hey, sou a promise');
+            resolve();
+        }, 2000);
+    });
+}
+
+export default async function() {
+    await promise();
+    console.log('terminou');
+}
