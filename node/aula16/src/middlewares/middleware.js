a2f5cfc8aa4e7ca0fc85525570646c4c68e52b16
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-03-18 20:54:47 -0300

    injetando conteudo no views

---
+++ node/aula16/src/middlewares/middleware.js
@@ -0,0 +1,26 @@
+// module.exports = (req, res, next) => {
+//     //console.log();
+//     //console.log('Passei no middleware global.');
+
+//     if(req.body.cliente) {
+//         req.body.cliente = req.body.cliente.replace('Carlos', 'Não use Carlos');//interceptei o valor alterei e ainda falei para o cliente nao usar
+//         console.log();
+//         console.log(`Vi que voce postou ${req.body.cliente}`);
+//         console.log();
+//     }
+
+//     //console.log();
+//     next();//sempre lembrar de chamar
+// };
+
+//outra forma de chamar o middleware global
+exports.middlewareGlobal = (req, res, next) => {
+    //injetando conteudo em todas as rotas
+    res.locals.umaVariavelLocal = 'Este é o valor da variável local.'
+    next();//sempre lembrar de chamar
+};
+
+exports.outroMiddleware = (req, res, next) => {
+    console.log('Sou seu outro middleware');
+    next();
+}
