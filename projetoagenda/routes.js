3ab0651cadc97bbeac161228c268eba22680f323
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2023-03-23 22:41:58 -0300

    projeto agenda  criando tela login

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -1,13 +1,12 @@
 const express = require('express');//carrega o express
 const route = express.Router();
 const homeController =require('./src/controllers/homeController') //importando homeController
-const contatoController = require('./src/controllers/contatoController');
+const loginController =require('./src/controllers/loginController') 
 
 //rotas da home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
+route.get('/', homeController.index);
 
-//rotas de contato
-route.get('/contato', contatoController.paginaInicial);
+//Rotas de login
+route.get('/login/index', loginController.index);
 
 module.exports = route;//sobreescrevi o modulo
--- projetoagenda/src/controllers/homeController.js
+++ projetoagenda/src/controllers/homeController.js
@@ -1,26 +1,6 @@
 
-exports.paginaInicial = (req, res) => {
-    //flash messages
-    //executa depois
-    //console.log(req.flash('error'), req.flash('sucess'), req.flash('info'));
+exports.index = (req, res) => {
+    res.render('index'); 
     
-    //executa primeiro
-    // req.flash('info', 'Olá Mundo!');
-    // req.flash('error', 'asdfasdf');
-    // req.flash('sucess', 'Galooooooo');
-
-    //console.log(req.session.usuario);
-    // req.session.usuario = { nome: 'Luiz', Logado: true };//ficara salvo por 7 dias
-    res.render('index', {
-        //titulo: 'Este será o <span style="color:red;">título</span> da página',
-        titulo: 'Este será o título da página',
-        numeros: [0, 1, 2, 3, 4 , 5, 6, 7, 8, 9]
-    });
-    return;
 };
 
-exports.trataPost = (req, res) => {
-    res.send(req.body);
-    //res.send('Ei, sou sua nova rota de de POST');
-    return;
-};
