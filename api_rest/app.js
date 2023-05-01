303a93a3d522d07bab1ad30486e3668e40787221
Luiz Carlos <cpjluiz01@gmail.com>
2023-05-01 19:46:40 -0300

    rotas de alunos

--- api_rest/app.js
+++ api_rest/app.js
@@ -8,6 +8,8 @@ import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
+import alunoRoutes from './src/routes/alunoRoutes';
+
 
 class App {
   constructor() {
@@ -24,7 +26,8 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
-    this.app.use('/tokens', tokenRoutes);
+    this.app.use('/tokens/', tokenRoutes);
+    this.app.use('/alunos/', alunoRoutes);
   }
 }
 
--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Luiz',
-      sobrenome: 'Pinto',
-      email: 'luizPinto@gmail.com',
-      idade: 300,
-      peso: 102,
-      altura: 1.8,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
