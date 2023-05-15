68c1f6d7f8a37eff9be55a31640110c66be847ad
Luiz Carlos <cpjluiz01@gmail.com>
2023-05-15 20:38:10 -0300

    componentes

---
+++ React/listatarefas/src/components/Main.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import './Main.css';
+
+export default class Main extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     novaTarefa: '',
+  //   };
+
+  //   this.inputMudou = this.inputMudou.bind(this);//dentro desse metodo o this é essa propria classe
+
+  // }
+  state = {
+        novaTarefa: '',//class fields
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  render() {
+    const { novaTarefa } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+
+    );
+  }
+}
