99d4fa8837c4cef33094bd88375b0d8e002f3ee2
Luiz Carlos <cpjluiz01@gmail.com>
2023-06-06 19:49:24 -0300

    configurando o Axios

--- React/react-base/src/pages/Login/index.js
+++ React/react-base/src/pages/Login/index.js
@@ -3,15 +3,27 @@ import React from "react";
 import { Container } from "../../styles/GlobalStyles";
 import { Title, Paragrafo } from './styled';
 
+// import axios from '../../services/axios';
+
 export default function Login() {
+  // React.useEffect(() => {
+  //   async function getData() {
+  //     const response = await axios.get('/alunos');
+  //     const { data } = response;
+  //     console.log(data);
+  //   }
+  //   getData();
+
+  // },[]);
   return (
+
     <Container>
     <Title>
       Login
       <small>Galo</small>
     </Title>
     <Paragrafo>Galo Forte Vingador</Paragrafo>
-  
+
     <button type='button'>Enviar</button>
     </Container>
   );
---
+++ React/react-base/src/services/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'Https://localhost:3001',
+});
