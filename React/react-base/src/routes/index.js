2f27da2019a977d8b40e26a66665e16cbb200fad
Luiz Carlos <cpjluiz01@gmail.com>
2023-06-05 21:17:49 -0300

    React Toastify

--- React/react-base/src/routes/index.js
+++ React/react-base/src/routes/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Switch } from "react-router-dom/cjs/react-router-dom";
+// import { toast } from 'react-toastify';
 
 import MyRoute from "./MyRoute";
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
+  // toast.success('Galo Campeão, sucesso');
+  // toast.error('Caibuloso rebaixado');
   return (
 
+
       <Switch>
-        <MyRoute exact path="/" component={Login} isClosed />
+        <MyRoute exact path="/" component={Login} />
         <MyRoute path="*" component={Page404} />
       </Switch>
 
