430d44ec7a9057c551091452c140cc5173a164aa
lskwr <lskwr@gmail.com>
2023-10-10 20:05:01 -0300

    aula 271 tipo any

---
+++ CURSOJSTYPESCRIPT/src/A0002-type-any/A0002-type-any.ts
@@ -0,0 +1,8 @@
+//utilize any apenas em ultimo caso
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage('ola'));
+console.log(showMessage(1));
