378fe34932551af57ef801cd5655746c29426eaf
lskwr <lskwr@gmail.com>
2023-10-17 21:07:27 -0300

    typescript  aula 278 tipo enum

---
+++ CURSOJSTYPESCRIPT/src/A0009-type-enum/A0009-type-enum.ts
@@ -0,0 +1,26 @@
+enum Cores {
+  VERMELHO, // 0 vermelho = 1 ; começa do 1
+  AZUL, // 1
+  AMARELO, // 2
+  //posso setar com os valores que quiser
+  ROXO = 'ROXO',
+  VERDE = 100,
+  PRETO, //AUTOMATICAMENTE PEGA O VALOR SUCESSIVO
+}
+
+enum Cores {
+  ROSA = 103, // O TS UNE OS VALORES EM UM UNICO ENUM
+}
+
+function escolhaCor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+console.log(Cores);
+console.log(Cores.VERMELHO);
+console.log(Cores[0]);
+console.log(Cores.ROXO);
+
+escolhaCor(Cores.PRETO); // chamada da funçao
+
+// enum -> estrutura de dados nao ordenada
