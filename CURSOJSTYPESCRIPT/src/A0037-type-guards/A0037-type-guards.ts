0d95febd09d4d5979b38aa1f1bbfad84aa54f992
lskwr <lskwr@gmail.com>
2023-12-06 23:31:24 -0300

    typescript aula 311 type guard => refinando tipos

---
+++ CURSOJSTYPESCRIPT/src/A0037-type-guards/A0037-type-guards.ts
@@ -0,0 +1,34 @@
+export function add(a: unknown, b: unknown): number | string {
+  return typeof a === 'number' && typeof b === 'number' ? a + b : `${a} + ${b}`;
+}
+
+console.log(add(1, 5));
+console.log(add('a', 'b'));
+
+// type Pessoa = { nome: string };
+// type Animal = { cor: string };
+type Pessoa = { tipo: 'pessoa'; nome: string };
+type Animal = { tipo: 'animal'; cor: string };
+type PessoaOuAnimal = Pessoa | Animal;
+
+export class Aluno implements Pessoa {
+  // eslint-disable-next-line @typescript-eslint/prefer-as-const
+  tipo: 'pessoa' = 'pessoa';
+  constructor(public nome: string) {}
+}
+
+function mostraNome(obj: PessoaOuAnimal): void {
+  //if ('nome' in obj) console.log(obj.nome);
+  //   if (obj instanceof Aluno) console.log(obj.nome);
+  switch (obj.tipo) {
+    case 'pessoa':
+      console.log(obj.nome);
+      return;
+    case 'animal':
+      console.log('Isso é um animal', obj.cor);
+      return;
+  }
+}
+
+mostraNome(new Aluno('Joao'));
+mostraNome({ tipo: 'animal', cor: 'Rosa' });
