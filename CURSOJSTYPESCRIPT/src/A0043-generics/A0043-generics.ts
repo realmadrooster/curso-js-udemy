4fae006543c5e6da0991dcfbdb89ea5d0d2bf10e
lskwr <lskwr@gmail.com>
2023-12-14 21:28:45 -0300

    typescript aula 317 introduçao ao tipos genericos

---
+++ CURSOJSTYPESCRIPT/src/A0043-generics/A0043-generics.ts
@@ -0,0 +1,25 @@
+type filterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
+
+export function meuFilter<T>(array: T[], callbackfn: filterCallback<T>): T[] {
+  const novoArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (callbackfn(array[i])) {
+      novoArray.push(array[i]);
+    }
+  }
+
+  return novoArray;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const arrayFiltradoOriginal = array.filter((value) => value < 5);
+
+console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = meuFilter(array, (value) => value < 5);
+
+console.log(arrayFiltrado);
+
+// qdo nao sei o que vou retornar uso generics <T>
