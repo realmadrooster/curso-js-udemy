ca79ee3c1c1343cc6415b64e2fd1fafd68ac268f
lskwr <lskwr@gmail.com>
2023-12-28 19:07:44 -0300

    typescript aula 320 exercicio votaçao

---
+++ CURSOJSTYPESCRIPT/src/A0051-exercicio-soluçao/A0051-exercicio-solucao.ts
@@ -0,0 +1,56 @@
+type VotationOption = {
+  numberOfvotes: number;
+  option: string;
+};
+
+export class Votation {
+  private _votationOptions: VotationOption[] = [];
+  constructor(public details: string) {}
+
+  addVotationOption(votationOption: VotationOption): void {
+    this._votationOptions.push(votationOption);
+  }
+  vote(votationIndex: number): void {
+    if (!this._votationOptions[votationIndex]) return;
+    this._votationOptions[votationIndex].numberOfvotes += 1;
+  }
+
+  get votationOptions(): VotationOption[] {
+    return this._votationOptions;
+  }
+}
+
+export class VotationApp {
+  private votations: Votation[] = [];
+
+  addVotation(votation: Votation): void {
+    this.votations.push(votation);
+  }
+
+  showVotations(): void {
+    for (const votation of this.votations) {
+      console.log(votation.details);
+      for (const votationOption of votation.votationOptions) {
+        console.log(votationOption.option, votationOption.numberOfvotes);
+      }
+      console.log('###');
+      console.log('');
+    }
+  }
+}
+
+const votation1 = new Votation('Qual a sua linguagem de programaçao favorita');
+votation1.addVotationOption({ option: 'Python', numberOfvotes: 0 });
+votation1.addVotationOption({ option: 'JavaScript', numberOfvotes: 0 });
+votation1.addVotationOption({ option: 'TypeScript', numberOfvotes: 0 });
+votation1.vote(0);
+votation1.vote(1);
+votation1.vote(1);
+votation1.vote(0);
+votation1.vote(2);
+votation1.vote(0);
+
+const votationApp = new VotationApp();
+votationApp.addVotation(votation1);
+
+votationApp.showVotations();
