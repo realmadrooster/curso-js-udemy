1b5a6fdc13935aad8999f20aa0e0452a436abbbc
lskwr <lskwr@gmail.com>
2024-01-02 16:38:40 -0300

    typescript aula331 decoradores de metodo

---
+++ CURSOJSTYPESCRIPT/src/A0056-decorators-method/A0056-decorators-method.ts
@@ -0,0 +1,45 @@
+function decorador(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  classPrototype: any,
+  nomeMetodo: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor | void {
+  console.log(classPrototype);
+  console.log(nomeMetodo);
+  console.log(descriptor);
+}
+
+export class UmaPessoa {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  @decorador
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ` ` + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Luiz', 'Carlos', 44);
+const metodo = pessoa.metodo(
+  'Olá mundo, preparado pra ser dominado pelo galão 2024?',
+);
+console.log(metodo);
