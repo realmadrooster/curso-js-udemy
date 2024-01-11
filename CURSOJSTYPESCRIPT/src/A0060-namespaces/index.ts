102eabb3c64b59e1886b7e1eb0140f5bc4e98030
lskwr <lskwr@gmail.com>
2024-01-11 18:31:27 -0300

    typescript aula 336 reference para imports de arquivos

--- CURSOJSTYPESCRIPT/src/A0060-namespaces/index.ts
+++ CURSOJSTYPESCRIPT/src/A0060-namespaces/index.ts
@@ -1,22 +1,4 @@
-/*eslint-disable @typescript-eslint/no-namespace */
-namespace MeuNameSpace {
-  export const nomeDoNameSpace = 'Luiz';
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="module.ts" />
 
-  export class PessoaDoNameSpace {
-    constructor(public nome: string) {}
-  }
-
-  const pessoaDoNamespace = new PessoaDoNameSpace('Luiz');
-  console.log(pessoaDoNamespace);
-
-  export namespace OutroNameSpace {
-    export const nomeDoNameSpace = 'Carlos Pinto Junior';
-  }
-}
-
-const pessoaDoNamespace = new MeuNameSpace.PessoaDoNameSpace('Carlos');
-console.log(pessoaDoNamespace);
 console.log(MeuNameSpace.nomeDoNameSpace);
-console.log(MeuNameSpace.OutroNameSpace.nomeDoNameSpace);
-
-export default 1;
---
+++ CURSOJSTYPESCRIPT/src/A0060-namespaces/module.ts
@@ -0,0 +1,20 @@
+/*eslint-disable @typescript-eslint/no-namespace */
+namespace MeuNameSpace {
+  export const nomeDoNameSpace = 'Luiz';
+
+  export class PessoaDoNameSpace {
+    constructor(public nome: string) {}
+  }
+
+  const pessoaDoNamespace = new PessoaDoNameSpace('Luiz');
+  console.log(pessoaDoNamespace);
+
+  export namespace OutroNameSpace {
+    export const nomeDoNameSpace = 'Carlos Pinto Junior';
+  }
+}
+
+const pessoaDoNamespace = new MeuNameSpace.PessoaDoNameSpace('Carlos');
+console.log(pessoaDoNamespace);
+console.log(MeuNameSpace.nomeDoNameSpace);
+console.log(MeuNameSpace.OutroNameSpace.nomeDoNameSpace);
